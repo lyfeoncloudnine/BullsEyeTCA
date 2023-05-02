1ef3e81484c50bd9c704bc6e0ee94a44b65bc586
lyfeoncloudnine <lyfeoncloudnine@gmail.com>
2023-05-02 19:52:45 +0900

    🎉 Add Project

---
+++ BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by lyfeoncloudnine on 2023/05/02.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
