44c761b4b82dc7f049395f3bc71b41aba65b2019
lyfeoncloudnine <lyfeoncloudnine@gmail.com>
2023-05-02 23:45:10 +0900

    ✅ Implement RecordService

--- BullsEye/Sources/Services/RecordServices/RecordService.swift
+++ BullsEye/Sources/Services/RecordServices/RecordService.swift
@@ -8,19 +8,40 @@
 import Foundation
 
 struct RecordService: RecordServiceType {
+    private let userDefaults = UserDefaults.standard
+    private let key = "recordServiceKey"
+    
     func records() -> [Record] {
-        []
+        if let data = userDefaults.data(forKey: key), let records = try? PropertyListDecoder().decode([Record].self, from: data) {
+            return records
+        } else {
+            return []
+        }
     }
     
-    func create(record: Record) -> [Record] {
-        []
+    @discardableResult func create(record: Record) -> [Record] {
+        var records = records()
+        records.append(record)
+        records.sort { $0.score > $1.score }
+        
+        userDefaults.set(try? PropertyListEncoder().encode(records), forKey: key)
+        
+        return records
     }
     
     func delete(record: Record) -> [Record] {
-        []
+        var records = records()
+        
+        guard records.isEmpty == false else { return records }
+        guard let index = records.firstIndex(where: { $0.id == record.id }) else { return records }
+        
+        records.remove(at: index)
+        userDefaults.set(try? PropertyListEncoder().encode(records), forKey: key)
+        
+        return records
     }
     
     func clear() {
-        
+        userDefaults.set(nil, forKey: key)
     }
 }
