6cea55bfce62799ad09b536a93f3cde9185c5c5c
lyfeoncloudnine <lyfeoncloudnine@gmail.com>
2023-05-03 17:12:48 +0900

    ✅ Implement RecordFeature

--- BullsEye/Sources/Features/Record/RecordFeature.swift
+++ BullsEye/Sources/Features/Record/RecordFeature.swift
@@ -11,16 +11,31 @@ import ComposableArchitecture
 
 struct RecordFeature: ReducerProtocol {
     enum Action: Equatable {
-        
+        case load
+        case delete(Record)
+        case setRecords([Record])
     }
     
     struct State: Equatable {
-        
+        var records = [Record]()
+        var title = "명예의 전당"
     }
     
     let recordService: RecordServiceType
     
     func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
-        
+        switch action {
+        case .load:
+            let records = recordService.records()
+            return .send(.setRecords(records))
+            
+        case .delete(let record):
+            let records = recordService.delete(record: record)
+            return .send(.setRecords(records))
+            
+        case .setRecords(let records):
+            state.records = records
+            return  .none
+        }
     }
 }
