aa8b15cdd82ffbfeb6576d9d604d6fcc5d231b86
lyfeoncloudnine <lyfeoncloudnine@gmail.com>
2023-05-03 17:13:14 +0900

    🚧 Add RecordView

---
+++ BullsEye/Sources/Features/Record/RecordView.swift
@@ -0,0 +1,20 @@
+//
+//  RecordView.swift
+//  BullsEye
+//
+//  Created by lyfeoncloudnine on 2023/05/03.
+//
+
+import SwiftUI
+
+struct RecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecordView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecordView()
+    }
+}
